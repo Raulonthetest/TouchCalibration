fc8d6ae4e411cb40d7dce1aff9a0a9ae942a6784
Raulonthetest <64203039+Raulonthetest@users.noreply.github.com>
2020-09-04 10:31:38 +0300

    Add project

---
+++ Calib/runcalibration.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SmartDeviceProject1
+{
+    partial class runcalibration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // runcalibration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "runcalibration";
+            this.Text = "Touch Calibration";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Calib/runcalibration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;                     //To use C Win32 functions
+
+//Source of TouchCalibrate function: https://developer.toradex.com/knowledge-base/touch-screen-calibration-windows-ce
+
+namespace SmartDeviceProject1
+{
+    public partial class runcalibration : Form
+    {
+        [DllImport("coredll.dll")]
+        private static extern bool TouchCalibrate();      //import function from coredll
+
+        [DllImport("coredll.dll", EntryPoint = "RegFlushKey", SetLastError = true)]
+        public static extern uint RegFlushKey(uint hKey); //import function from coredll
+
+        const uint HKEY_LOCAL_MACHINE = 0x80000002;
+
+        public runcalibration()
+        {
+            InitializeComponent();
+            touchCalibrate();// function will be called on Form Load
+            this.Close();
+        }
+        public void touchCalibrate()
+        {
+            TouchCalibrate();
+            RegFlushKey(HKEY_LOCAL_MACHINE);
+        }
+    }
+}
